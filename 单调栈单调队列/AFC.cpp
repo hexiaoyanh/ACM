650929605fd58604878ec7cd11e0b6e52b19304e
mika <heiaoyang@foxmail.com>
2018-06-11 14:31:32 +0800

    update

--- 单调栈单调队列/AFC.cpp
+++ 单调栈单调队列/AFC.cpp
@@ -1,12 +1,45 @@
+/*
+TLE 不知道为什么
+*/
 #include <iostream>
+#include <cstring>
 using namespace std;
 int main()
 {
+#define int long long
 	int n;
+	int cases=1;
 	while(cin>>n)
 	{
+		int s=n;
 		int bd[n+5];
-
+		int top=0;
+		memset(bd,-1,sizeof(bd));
+		for(int i=0;i<n;++i)
+		{
+			int h;
+			scanf("%lld",&h);
+			if(h<0)continue;
+			if(h==0)
+			{
+				--s;
+				top=0;
+				memset(bd,-1,sizeof(bd));
+				continue;
+			}
+			if(h>bd[top])
+				bd[++top]=h;
+			else
+			{
+				while(h<=bd[top])
+				{
+					bd[top]=-1;
+					--top;
+				}
+				bd[++top]=h;
+				s--;
+			}
+		}	
+		printf("Case %lld: %lld\n",cases++,s);
 	}
 }
-
--- 比赛专用/main2.cpp
+++ 比赛专用/main2.cpp
@@ -1,51 +1,13 @@
-#include<stdio.h>
-#include<algorithm>
-#define MAX_N 100009
+#include <iostream>
+#include <algorithm>
+#include <cstring>
 using namespace std;
-int rect[MAX_N];
-struct seg
+#define ll unsigned long long
+int main() 
 {
-    long long  value,s;
-}q[MAX_N];
-int main()
-{
-    int n ;
-    while(scanf("%d",&n)&&n)
-    {
-        long long area = 0 ,top = 0;
-        for(int i=0;i!=n;++i)
-            scanf("%d",rect+i);
-        for(int i=0;i!=n;++i)
-        {
-            //队列为空或者当前元素大于队尾时入队
-            if(!top || q[top-1].value < rect[i])
-            {
-                q[top].value = rect[i];
-                q[top++].s = i ;
-            }
-            //此时，右边界出现，需要做更新
-            else if(q[top-1].value>rect[i])
-            {
-                //如果有比最大值大的面积，则更新最大值
-                while(top && q[top-1].value>rect[i])
-                {
-                    --top;
-                    area = max(area,q[top].value*(i-q[top].s));
-                }
-                //队空
-                if(!top)
-                    q[top++].value = rect[i] ;
-                else if(q[top-1].value < rect[i])
-                    q[top++].value = rect[i] ;
-            }
-        }
-        //队中可能还有元素，也要做相应的更新
-        while(top)
-        {
-            --top;
-            area = max(area,q[top].value*(n-q[top].s));
-        }
-        printf("%lld\n",area);
-    }
-    return 0;
+	ll n;
+	while(cin>>n)
+	{		
+		
+	}	
 }
